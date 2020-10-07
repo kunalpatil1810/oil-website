df0c68740c0491b4b56a493fb8ac2880b5a834bc
Yash Sonar <yashsonar213@gmail.com>
2020-10-08 02:54:32 +0530

    menu options animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -139,7 +139,41 @@
       }
     });
   });
+
+    // Back to top button
+    $(window).scroll(function() {
+      if ($(this).scrollTop() > 100) {
+        $('.back-to-top').fadeIn('slow');
+      } else {
+        $('.back-to-top').fadeOut('slow');
+      }
+    });
   
+    $('.back-to-top').click(function() {
+      $('html, body').animate({
+        scrollTop: 0
+      }, 1500, 'easeInOutExpo');
+      return false;
+    });
+  
+    // Menu list isotope and filter
+    $(window).on('load', function() {
+      var menuIsotope = $('.menu-container').isotope({
+        itemSelector: '.menu-item',
+        layoutMode: 'fitRows'
+      });
+  
+      $('#menu-flters li').on('click', function() {
+        $("#menu-flters li").removeClass('filter-active');
+        $(this).addClass('filter-active');
+  
+        menuIsotope.isotope({
+          filter: $(this).data('filter')
+        });
+        aos_init();
+      });
+    });
+
   // Events carousel (uses the Owl Carousel library)
   $(".events-carousel").owlCarousel({
     autoplay: true,
